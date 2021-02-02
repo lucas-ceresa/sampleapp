7f90bf1e6b38ef87b2ce4bfda230c50d116c50fd
Lucas Ceresa <lucas.ceresa10@gmail.com>
2021-02-01 22:24:24 -0300

    Cambio en .js

--- home.js
+++ home.js
@@ -1,11 +1,1 @@
-const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
-
-const result = words.filter(word => word.length > 6);
-
-document.write(result);
-
-const result2 = words.forEach(function(word){
-    return word;
-});
-
-document.write(" " + result2);
+console.log('hola que tal')
